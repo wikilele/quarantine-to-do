76ce56723a3ae3c3ff1fc3d94e2e639c1c32f61d
leonardo <l.frioli@studenti.unipi.it>
2020-04-10 12:57:49 +0200

    now the old repo should be deleted

--- web-node-backend/controllers/quotes-controller.js
+++
@@ -1,8 +0,0 @@
-const model = require('../models/quotes.js')
-
-function getQuote () {
-  console.log(model.quotes)
-  return model.quotes[Math.floor(Math.random() * model.quotes.length)]
-}
-
-exports.getQuote = getQuote
--- web-node-backend/models/quotes.js
+++
@@ -1,2 +0,0 @@
-
-exports.quotes = ['today is the day', 'carpe diem', 'valar morghulis', 'they are taking the hobbits to Isengard']
