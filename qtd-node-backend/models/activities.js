69f95786e1f86a60bb81cfcbc265acdd20ca6cf6
leonardo <l.frioli@studenti.unipi.it>
2020-04-10 13:18:59 +0200

    changed quotes sentences

--- qtd-node-backend/models/activities.js
+++ qtd-node-backend/models/activities.js
@@ -1,2 +1,2 @@
 
-exports.activities = ['today is the day', 'carpe diem', 'valar morghulis', 'they are taking the hobbits to Isengard']
+exports.activities = ['read a book', 'watch a film', 'play video-games', 'watch The Lord Of The Rings', 'study']
