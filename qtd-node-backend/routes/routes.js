41b7ea45222e862f2677cb6c3a1b461dbb79378a
leonardo <l.frioli@studenti.unipi.it>
2020-04-11 18:02:47 +0200

    cleaned comments

--- qtd-node-backend/routes/routes.js
+++ qtd-node-backend/routes/routes.js
@@ -6,25 +6,20 @@ const cors = require('cors')
 const api = express()
 api.use(cors())
 
-// configure app to use bodyParser()
 // this will let us get the data from a POST
 api.use(bodyParser.urlencoded({ extended: true }))
 api.use(bodyParser.json())
 
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router() // get an instance of the express Router
+var router = express.Router() 
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+// API ROUTES
 router.get('/activity', function (req, res) {
   console.log(activitiesController.getActivity())
   var randActivity = activitiesController.getActivity()
   res.json({ activity: randActivity })
 })
+//=========================================================
 
-// more routes for our API will happen here
-
-// REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
 api.use('/api', router)
 
--- qtd-node-backend/server.js
+++ qtd-node-backend/server.js
@@ -1,8 +1,6 @@
 const app = require('./routes/routes.js')
 var port = process.env.PORT || 4242 // set our port
 
-// START THE SERVER
-// =============================================================================
 app.api.listen(port)
 console.log('localhost:' + port)
 
