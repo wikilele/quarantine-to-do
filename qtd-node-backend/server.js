8549373fb702d83420cbc8020a05d35563ffb179
leonardo <l.frioli@studenti.unipi.it>
2020-04-11 12:35:57 +0200

    added test to backend

--- qtd-node-backend/server.js
+++ qtd-node-backend/server.js
@@ -1,7 +1,9 @@
-const app = require('./routes/routes.js')
-var port = process.env.PORT || 4242 // set our port
+const app = require('./routes/routes.js');
+var port = process.env.PORT || 4242; // set our port
 
 // START THE SERVER
 // =============================================================================
-app.api.listen(port)
-console.log('localhost:' + port)
+app.api.listen(port);
+console.log('localhost:' + port);
+
+module.exports = app.api;
---
+++ qtd-node-backend/test/api.test.js
@@ -0,0 +1,21 @@
+const app = require("../server.js");
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const chaiJson = require("chai-json");
+
+const { expect } = chai;
+chai.use(chaiHttp);
+chai.use(chaiJson);
+
+describe("get actvitiry test", () => {
+    it("gets the activity sentence", done =>{
+        chai.request(app)
+            .get("/api/activity")
+            .end((err,res) => {
+                expect(res).to.have.status(200);
+                expect(res.body.activity).to.be.a('string');
+
+                done();
+            });
+    });
+});
