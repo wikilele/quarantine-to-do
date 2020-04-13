5a341da629fafe7cc75e8d6a866c69a660b32202
leonardo <l.frioli@studenti.unipi.it>
2020-04-13 18:13:44 +0200

    added other buttons, need to ceter better the to-do div

--- qtd-ng-frontend/src/app/to-do/to-do.component.ts
+++ qtd-ng-frontend/src/app/to-do/to-do.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
 @Component({
   selector: 'app-to-do',
   templateUrl: './to-do.component.html',
@@ -8,26 +7,40 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class ToDoComponent implements OnInit {
 
-  activity:string;
+  message:string;
   url:string = 'http://localhost:4242/api/activity'
-
+  
   constructor(
     private http: HttpClient
   ) {
-    this.activity = "!push the button to get something to do!"
+    this.message = "!push the button to get something to do!"
    }
 
   ngOnInit() {
-  }
+  } 
 
   getActivity(){
     var httpHeaders : HttpHeaders = new HttpHeaders({
       'Content-Type': 'application/json',
     });
     this.http.get<JsonActivity>(this.url, {headers : httpHeaders})
-             .subscribe(q => this.activity = q.activity);
+             .subscribe(q => this.message = q.activity );
   }
+
+  suggestActivity(){
+    this.message = "write an email to user@mail.com to suggest an activity"
+  }
+
+  shareActivityOnFacebook(){
+    this.message = "share on facebook"
+  }
+
+  shareActivityOnTwitter(){
+    this.message = "share on twitter"
+  }
+
 }
+
 
 interface JsonActivity  {
   activity : string;
