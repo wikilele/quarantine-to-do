18d56d28f73e8caa21dba0ddf98226ec8807d612
leonardo <l.frioli@studenti.unipi.it>
2020-04-15 15:54:08 +0200

    compose is almost ready

--- qtd-node-backend/config/config.js
+++ qtd-node-backend/config/config.js
@@ -6,7 +6,7 @@ config.db = {}
 config.api.host = 'localhost'
 config.api.port = process.env.PORT || 4201
 
-config.db.host = 'localhost'
+config.db.host = 'localhost' // 'mongo'
 config.db.port = 4202 // 27017
 config.db.name = 'qtd-mongo-db'
 
