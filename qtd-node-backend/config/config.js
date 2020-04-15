2ead1d4a97a493c0f18d79358f4a32f9d2ffe80d
leonardo <l.frioli@studenti.unipi.it>
2020-04-15 12:54:12 +0200

    removed db

--- qtd-node-backend/config/config.js
+++ qtd-node-backend/config/config.js
@@ -1,5 +1,3 @@
-const yaml = require('js-yaml')
-const fs = require('fs')
 
 var config = {}
 config.api = {}
@@ -9,17 +7,7 @@ config.api.host = 'localhost'
 config.api.port = process.env.PORT || 4201
 
 config.db.host = 'localhost'
-config.db.port = 27017
+config.db.port = 4202 // 27017
 config.db.name = 'qtd-mongo-db'
-try {
-  const fileContents = fs.readFileSync('./config/mongo.config.yaml', 'utf8')
-  const mongoconfig = yaml.safeLoad(fileContents)
-
-  config.db.host = mongoconfig.net.bindIp
-  config.db.port = mongoconfig.net.port
-} catch (e) {
-  console.log(e)
-  // the we will connect to the db using the default values
-}
 
 module.exports = config
