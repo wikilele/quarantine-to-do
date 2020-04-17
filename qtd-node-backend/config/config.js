341e6acea5e4b2c8bfdc9979f03f60207afc7945
leonardo <l.frioli@studenti.unipi.it>
2020-04-17 13:05:40 +0200

    small changes

--- qtd-node-backend/config/config.js
+++ qtd-node-backend/config/config.js
@@ -3,7 +3,7 @@ var config = {}
 config.api = {}
 config.db = {}
 
-config.api.host = 'localhost'
+config.api.host = process.env.API_HOST || 'localhost'
 config.api.port = process.env.API_PORT || 4201
 
 config.db.host = process.env.DB_HOST || 'localhost' // 'mongo'
